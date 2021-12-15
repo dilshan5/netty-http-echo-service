7b8e312353edf1399b9f0c66e1b348c4e151763b
dilshanf ernando <dilshan.fernando@syscolabs.com>
2021-12-15 10:42:49 +0530

    Add the request headers to the response body

--- src/main/java/com/performance/netty/echo/EchoHttpServerHandler.java
+++ src/main/java/com/performance/netty/echo/EchoHttpServerHandler.java
@@ -1,5 +1,7 @@
 package com.performance.netty.echo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
@@ -7,9 +9,10 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http2.HttpConversionUtil;
-
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
@@ -26,6 +29,7 @@ public class EchoHttpServerHandler extends SimpleChannelInboundHandler<FullHttpR
 
     private long sleepTime;
     private boolean h2AggregateContent;
+    private static String remoteHost;
 
     EchoHttpServerHandler(long sleepTime, boolean h2AggregateContent) {
         this.sleepTime = sleepTime;
@@ -34,7 +38,8 @@ public class EchoHttpServerHandler extends SimpleChannelInboundHandler<FullHttpR
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) {
-        request.headers().set("Backend-IN-time", OffsetDateTime.now(ZoneOffset.UTC));
+        //    request.headers().set("Backend-IN-time", OffsetDateTime.now(ZoneOffset.UTC));
+        remoteHost = ((InetSocketAddress)ctx.channel().remoteAddress()).getAddress().getHostAddress();
         if (h2AggregateContent) {
             String streamId = request.headers().get(HttpConversionUtil.ExtensionHeaderNames.STREAM_ID.text());
             FullHttpResponse response = buildFullHttpResponse(request);
@@ -67,17 +72,44 @@ public class EchoHttpServerHandler extends SimpleChannelInboundHandler<FullHttpR
 
     private static FullHttpResponse buildFullHttpResponse(FullHttpRequest request) {
         FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, request.content().copy());
-        response.headers().set("Backend-OUT-time", OffsetDateTime.now(ZoneOffset.UTC));
+
+        //print request headers to response body as a json if the request body is empty
+        if (request.content().readableBytes() == 0) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            Set<String> headerNames = request.headers().names();
+            for (String key : headerNames) {
+                String value = request.headers().get(key);
+                map.put(key, value);
+            }
+            map.put("Remote-address", remoteHost);
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                String json = objectMapper.writeValueAsString(map);
+                byte[] byteArr = json.getBytes();
+                response.content().writeBytes(byteArr);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        response.headers().set("syy-operation-id", "getNettyEchoResponse");
+
         String contentType = request.headers().get(HttpHeaderNames.CONTENT_TYPE);
         if (contentType != null) {
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, contentType);
-        }
+        } else
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
+
         response.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+
+/*        response.headers().set("Backend-OUT-time", OffsetDateTime.now(ZoneOffset.UTC));
         if (request.headers().get("Jmeter-OUT-time") != null)
             response.headers().set("Jmeter-OUT-time", request.headers().get("Jmeter-OUT-time"));
         response.headers().set("Backend-IN-time", request.headers().get("Backend-IN-time"));
         if (request.headers().get("APIC-request-id") != null)
-            response.headers().set("APIC-request-id", request.headers().get("APIC-request-id"));
+            response.headers().set("APIC-request-id", request.headers().get("APIC-request-id"));*/
+
         return response;
     }
+
 }
